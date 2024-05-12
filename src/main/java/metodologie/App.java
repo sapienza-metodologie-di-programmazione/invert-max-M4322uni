e39d5113af0a40d20f3fdc08f3bf4e56a4eadbd8
M4322 <piscitello.2130445@studenti.uniroma1.it>
2024-05-12 10:51:50 +0100

    Possibile soluzione per l'esercizio

--- src/main/java/metodologie/App.java
+++ src/main/java/metodologie/App.java
@@ -1,14 +1,33 @@
 package metodologie;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
     }
 
     public static <T> Invert<T> inverter() {
-        return null;
+        return new Invert<T>() {
+
+			@Override
+			public List<T> invert(List<T> list) {
+				List<T> neu = new ArrayList<>(list);
+				Collections.reverse(neu);
+				return neu;
+			};
+        
+        };
     }
 
-    public static <T> Max<T> maximizer() {
-        return null;
+    public static <T extends Comparable<T>> Max<T> maximizer() {
+        return new Max<T>() {
+
+			@Override
+			public T max(List<T> list) {
+				return Collections.max(list);
+			}
+        	
+        };
     }
 }
